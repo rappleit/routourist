23c0695d06634c741ff28d91f1747f7397c8ac78
Asyraf Omar <asyrafomar68@gmail.com>
2023-05-23 07:09:04 +0800

    Business content added

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,12 +4,29 @@ import travel from '../images/travel.svg';
 import firebase_logo from '../images/firebase_logo.png';
 import map_logo from '../images/maps_logo.png';
 import cloud_logo from '../images/cloud_logo.png';
+import environment from '../images/environment_logo.png';
+import economic from '../images/economic_logo.png';
+import ethics from '../images/ethics_logo.png';
+import social from '../images/social_logo.png';
 import {BsCheckCircleFill} from 'react-icons/bs';
 import { useState,useEffect } from 'react';
 
 const Landing = () => {
-    const [isBusiness,setIsBusiness] = useState(false);
+    const businessContent = document.getElementsByClassName("business_section")[0];
+    const consumerContent = document.getElementsByClassName("consumer_section")[0];
 
+    const businessButton = document.getElementsByClassName("business");
+    const consumerButton = document.getElementsByClassName("consumer");
+    const setBusiness = () => {
+        console.log(businessContent)
+        businessContent.style.display = 'block'
+        consumerContent.style.display = 'none'
+    }
+
+    const setConsumer = () => {
+        businessContent.style.display = 'none'
+        consumerContent.style.display = 'block'
+    }
     return ( 
         
         <div className="landingPage">
@@ -73,25 +90,82 @@ const Landing = () => {
             </div>
 
             <div className="switch_section">
-                <button className='consumer'>Consumer</button>
-                <button className='business'>Business</button>
+                <button className='consumer' onClick={()=> setConsumer()}>Consumer</button>
+                <button className='business' onClick={()=> setBusiness()}>Business</button>
+            </div>
+            <div className='consumer_section'>
+                <div className="sus_section">
+                    <h1>We aspire Sustainable Tourism.</h1>
+                    <p>We made your travelling more sustainble to enchance your experience
+                        and the future of tourism.
+                    </p>
+
+                    <div className='sus_sub'>
+                        <div className='sus_sub1'>
+                            <img src={environment}></img>
+                            <p>Environmental: Reduce your carbon footprint and help you to recycle your trash whenever you can.</p>
+                        </div>
+                        <div className='sus_sub1'>
+                            <img src={social}></img>
+                            <p>Social: Immerse yourself in an authentic once in a lifetime experience and learn their traditions/laws.</p>
+                        </div>
+                        <div className='sus_sub1'>
+                            <img src={economic}></img>
+                            <p>Economic: Support the local businesses </p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="comment_section">
+
+                </div>
+
+                <div className="procedure_section">
+
+                </div>
+
+                <div className="callToAction_section">
+
+                </div>
             </div>
 
-            <div className="features_section">
+            <div className='business_section'>
+                <div className="sus_section">
+                    <h1>Boost your sustainable business with our platform</h1>
+                    <p>We want to support sustainable businesses by increasing exposure of their products and services to our users.
+                    </p>
 
+                    <div className='sus_sub'>
+                        <div className='sus_sub1'>
+                            <img src={environment}></img>
+                            <p>Environmental: Products and Services that are made through sustainable means or promotes sustainability</p>
+                        </div>
+                        <div className='sus_sub1'>
+                            <img src={social}></img>
+                            <p>Social: Products and Services that have cultural significance.</p>
+                        </div>
+                        <div className='sus_sub1'>
+                            <img src={ethics}></img>
+                            <p>Economic: Ethically sourced products and services that prioritize fair labor practices.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="comment_section">
+
+                </div>
+
+                <div className="procedure_section">
+
+                </div>
+
+                <div className="callToAction_section">
+
+                </div>
             </div>
-
-            <div className="comment_section">
-
-            </div>
-
-            <div className="procedure_section">
-
-            </div>
-
-            <div className="callToAction_section">
-
-            </div>
+            
 
             <div className="footer_section">
 
